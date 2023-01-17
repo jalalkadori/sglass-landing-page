d5b351812efcc8fb740fca67269b8975663fa836
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-01-17 12:15:39 +0100

    updating script file

--- main.js
+++ main.js
@@ -56,3 +56,39 @@
         }, 2000);
     
     })();
+
+// whatsapp button 
+var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?3254';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    var options = {
+        "enabled":true,
+        "chatButtonSetting":{
+            "backgroundColor":"#4dc247",
+            "ctaText":"Contact Nous",
+            "borderRadius":"25",
+            "marginLeft":"0",
+            "marginBottom":"50",
+            "marginRight":"50",
+            "position":"right"
+        },
+        "brandSetting":{
+            "brandName":"JKAD",
+            "brandSubTitle":"Typically replies within a day",
+            "brandImg":"https://cdn.clare.ai/wati/images/WATI_logo_square_2.png",
+            "welcomeText":"Hi there!\nHow can I help you?",
+            "messageText":"Hello, I have a question about {{page_link}}",
+            "backgroundColor":"#0a5f54",
+            "ctaText":"Start Chat",
+            "borderRadius":"25",
+            "autoShow":false,
+            "phoneNumber":"212601949570"
+        }
+    };
+    s.onload = function() {
+        CreateWhatsappChatWidget(options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
